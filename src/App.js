1bb76abcda89effda2f18a70296cdb2178ef6736
Cavs Zhouyou <zhouyou.world@outlook.com>
2020-02-04 18:06:45 +0800

    chore: 添加数据请求

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { PureComponent } from "react";
 import logo from "./logo.svg";
+import axios from "axios";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <span className="App-link">前端部署实验</span>
-      </header>
-    </div>
-  );
+class App extends PureComponent {
+  state = {
+    title: ""
+  };
+
+  componentDidMount() {
+    const self = this;
+    axios.get("/api/test").then(function(response) {
+      console.log(response);
+
+      self.setState({
+        title: response.data
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <span className="App-link">{this.state.title}</span>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
